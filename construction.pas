2a8805e5b76d7771a50f93b0f658b239dd3e124f
Spinjitzu <98660278+Spinjitzu@users.noreply.github.com>
2025-11-26 16:05:49 +0100

    Construction

---
+++ construction.pas
@@ -0,0 +1,139 @@
+unit Construction;
+
+interface
+
+uses SysUtils, GestionEcran, utils, Logique, Resources, Emplacement;
+
+type
+  TypeConstructions = (
+    mine,
+    constructeur,
+    hub,
+    centrale_elec,
+    ascenseur_orbitale
+    );
+
+  TConstruction = record // Je créer un record complet pour les constructions
+    Nom: string;
+    CoutConstruction: array of record
+      Ressource: string;
+      Quantite: integer;
+    end;
+    EnergieConsommee: integer;
+    CoutAmelioration: array of array of record
+      Ressource: string;
+      Quantite: integer;
+    end;
+  end;
+
+var
+  Constructions: array[TypeConstructions] of TConstruction;
+
+implementation 
+
+
+unit Construction;
+
+interface
+
+uses SysUtils, GestionEcran, utils, Logique, Resources, Emplacement;
+
+type
+  TypeConstructions = (
+    mine,
+    constructeur,
+    hub,
+    centrale_elec,
+    ascenseur_orbitale
+  );
+
+  TConstruction = record
+    Nom: string;
+    CoutConstruction: array of record
+      Ressource: string;
+      Quantite: Integer;
+    end;
+    EnergieConsommee: Integer;
+    CoutAmelioration: array of array of record
+      Ressource: string;
+      Quantite: Integer;
+    end;
+  end;
+
+var
+  Constructions: array[TypeConstructions] of TConstruction;
+
+procedure InitialiserConstructions;
+
+implementation
+
+procedure InitialiserConstructions;
+begin
+  // Mine
+  Constructions[mine].Nom := 'Mine Mk.';
+  SetLength(Constructions[mine].CoutConstruction, 1);
+  Constructions[mine].CoutConstruction[0].Ressource := 'plaques_de_fer';
+  Constructions[mine].CoutConstruction[0].Quantite := 10;
+  Constructions[mine].EnergieConsommee := 100;
+  SetLength(Constructions[mine].CoutAmelioration, 3);
+  SetLength(Constructions[mine].CoutAmelioration[0], 2);
+  Constructions[mine].CoutAmelioration[0][0].Ressource := 'plaques_de_fer';
+  Constructions[mine].CoutAmelioration[0][0].Quantite := 20;
+  Constructions[mine].CoutAmelioration[0][1].Ressource := 'sacs_de_beton';
+  Constructions[mine].CoutAmelioration[0][1].Quantite := 20;
+  SetLength(Constructions[mine].CoutAmelioration[1], 2);
+  Constructions[mine].CoutAmelioration[1][0].Ressource := 'plaques_de_fer';
+  Constructions[mine].CoutAmelioration[1][0].Quantite := 20;
+  Constructions[mine].CoutAmelioration[1][1].Ressource := 'acier';
+  Constructions[mine].CoutAmelioration[1][1].Quantite := 20;
+
+  // Constructeur
+  Constructions[constructeur].Nom := 'Constructeur';
+  SetLength(Constructions[constructeur].CoutConstruction, 2);
+  Constructions[constructeur].CoutConstruction[0].Ressource := 'cables_de_cuivre';
+  Constructions[constructeur].CoutConstruction[0].Quantite := 10;
+  Constructions[constructeur].CoutConstruction[1].Ressource := 'plaques_de_fer';
+  Constructions[constructeur].CoutConstruction[1].Quantite := 10;
+  Constructions[constructeur].EnergieConsommee := 200;
+  SetLength(Constructions[constructeur].CoutAmelioration, 2);
+  SetLength(Constructions[constructeur].CoutAmelioration[0], 2);
+  Constructions[constructeur].CoutAmelioration[0][0].Ressource := 'plaques_de_fer';
+  Constructions[constructeur].CoutAmelioration[0][0].Quantite := 20;
+  Constructions[constructeur].CoutAmelioration[0][1].Ressource := 'sacs_de_beton';
+  Constructions[constructeur].CoutAmelioration[0][1].Quantite := 20;
+  SetLength(Constructions[constructeur].CoutAmelioration[1], 2);
+  Constructions[constructeur].CoutAmelioration[1][0].Ressource := 'plaques_de_fer';
+  Constructions[constructeur].CoutAmelioration[1][0].Quantite := 20;
+  Constructions[constructeur].CoutAmelioration[1][1].Ressource := 'acier';
+  Constructions[constructeur].CoutAmelioration[1][1].Quantite := 20;
+
+  // HUB
+  Constructions[hub].Nom := 'HUB';
+  Constructions[hub].EnergieConsommee := 100;
+  SetLength(Constructions[hub].CoutConstruction, 0);
+  SetLength(Constructions[hub].CoutAmelioration, 0);
+
+  // Centrale électrique
+  Constructions[centrale_elec].Nom := 'Centrale électrique';
+  SetLength(Constructions[centrale_elec].CoutConstruction, 3);
+  Constructions[centrale_elec].CoutConstruction[0].Ressource := 'cables_de_cuivre';
+  Constructions[centrale_elec].CoutConstruction[0].Quantite := 30;
+  Constructions[centrale_elec].CoutConstruction[1].Ressource := 'plaques_de_fer';
+  Constructions[centrale_elec].CoutConstruction[1].Quantite := 10;
+  Constructions[centrale_elec].CoutConstruction[2].Ressource := 'sacs_de_beton';
+  Constructions[centrale_elec].CoutConstruction[2].Quantite := 20;
+  Constructions[centrale_elec].EnergieConsommee := -1200; // produit 1200 d’énergie
+
+  // Ascenseur orbital
+  Constructions[ascenseur_orbitale].Nom := 'Ascenseur orbital';
+  SetLength(Constructions[ascenseur_orbitale].CoutConstruction, 3);
+  Constructions[ascenseur_orbitale].CoutConstruction[0].Ressource := 'cables_de_cuivre';
+  Constructions[ascenseur_orbitale].CoutConstruction[0].Quantite := 200;
+  Constructions[ascenseur_orbitale].CoutConstruction[1].Ressource := 'plaques_de_fer';
+  Constructions[ascenseur_orbitale].CoutConstruction[1].Quantite := 200;
+  Constructions[ascenseur_orbitale].CoutConstruction[2].Ressource := 'sacs_de_beton';
+  Constructions[ascenseur_orbitale].CoutConstruction[2].Quantite := 200;
+  Constructions[ascenseur_orbitale].EnergieConsommee := 1000;
+end;
+
+end.
--- emplacement.pas
+++ emplacement.pas
@@ -1,22 +1,17 @@
-unit emplacement;
+unit Emplacement;
 
 interface
 
-uses SysUtils, GestionEcran, utils, logique;
-
-const
-  FER_DEPART = 200;
+uses SysUtils, GestionEcran, utils, Logique, Resources, Construction;
 
 type
-  emplacement = record
+  Emplacement = record
     id: integer;          // de 1 à 10
     niveau: integer;      // 1 à 3
     decouvert: boolean;   // Si True alors découvert sinon non
     gisement: boolean;    // Si True alors possibilité de construire une mine
-    typologie: string;
-    // Mine, Constructeur, Central, AscenseurOrbital ou Hub (le hub est unique)
-    minerai: string;      // Si gisement alors fer, calcaire ou cuivre
-    energieConso: integer;
+    typologie: TypeConstructions;
+    minerai: resources;   // Si gisement alors fer, calcaire ou cuivre
   end;
 
 
