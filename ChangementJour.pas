7fe66bdb072a77c22caa9614d9a573374a7ff004
Yassir/Spin <azargyassir2@gmail.com>
2025-11-30 12:11:46 +0100

    Jours

--- ChangementJour.pas
+++ ChangementJour.pas
@@ -3,9 +3,47 @@ unit ChangementJour;
 
 interface
 
+
+// Fonction qui retourne le label du jour
+function getJourLabel(jour : Jours) : String;
+
+
 uses sysutils;
+
+type
+  Jours = (
+    lundi,
+    mardi,
+    mercredi,
+    jeudi,
+    vendredi,
+    samedi,
+    dimanche,
+  );
+
+
 
 implementation
 
-type 
+const 
+  JoursLabels: array[Jours] of string = (
+    'Lundi',
+    'Mardi',
+    'Mercredi',
+    'Jeudi',
+    'Vendredi',
+    'Samedi',
+    'Dimanche'
+  );
+
+//@param jour Jours le jour volu
+//return String le label du jour
+function getJourLabel(jour : Jours) : String;
+  begin
+    getJourLabel := JoursLabels[jour];
+  end;
+
+
+end.
+ 
   
